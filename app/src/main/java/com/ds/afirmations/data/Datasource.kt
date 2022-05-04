f535136fd1e9b9abbf78eeb967752fa345afb316
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-04 20:50:12 -0300

    [Aula 04/05/22] Adicionando uma classe de dados Datasource com uma função que retorna a lista de Affirmations

---
+++ app/src/main/java/com/ds/afirmations/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.ds.afirmations.data
+
+import com.ds.afirmations.R
+import com.ds.afirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation>{
+        return listOf<Affirmation>(
+                 Affirmation(R.string.affirmation1),
+                 Affirmation(R.string.affirmation2),
+                 Affirmation(R.string.affirmation3),
+                 Affirmation(R.string.affirmation4),
+                 Affirmation(R.string.affirmation5),
+                 Affirmation(R.string.affirmation6),
+                 Affirmation(R.string.affirmation7),
+                 Affirmation(R.string.affirmation8),
+                 Affirmation(R.string.affirmation9),
+                 Affirmation(R.string.affirmation10)
+                )
+    }
+}
