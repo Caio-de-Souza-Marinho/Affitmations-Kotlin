48adc87c37af2df33cccc4e20848394e37f04808
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-04 21:32:16 -0300

    [Aula 04/05/22] Adicionando textView com o tamanho da lista no Datasource para confirmar se está funcionando

--- app/src/main/java/com/ds/afirmations/MainActivity.kt
+++ app/src/main/java/com/ds/afirmations/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ds.afirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.ds.afirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView1)
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
