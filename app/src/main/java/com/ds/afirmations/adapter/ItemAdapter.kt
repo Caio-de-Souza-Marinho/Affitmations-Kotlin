08697e3bacbb8f62b7cb2253b19fabc644f882c2
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-11 21:23:41 -0300

    [Aula 11/05/22] Adicionando novo pacote com a classe ItemAdapter

---
+++ app/src/main/java/com/ds/afirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,7 @@
+package com.ds.afirmations.adapter
+
+import android.content.Context
+import com.ds.afirmations.model.Affirmation
+
+class ItemAdapter (private val context: Context, private val dataset: List<Affirmation>) {
+}
