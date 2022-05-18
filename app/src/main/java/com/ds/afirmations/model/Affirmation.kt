7db4f1b12be0daed9c62f8aea7bb35595a0ae84c
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-18 20:48:04 -0300

    [Aula 18/05/22] Adicionando imagens no Drawable e alterando o construtor da classe Affirmations

--- app/src/main/java/com/ds/afirmations/model/Affirmation.kt
+++ app/src/main/java/com/ds/afirmations/model/Affirmation.kt
@@ -1,5 +1,9 @@
 package com.ds.afirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 // adicionando a palavra chave data antes de class transforma em uma classe de dados
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
+
 }
